da44da532306f52de260cb35080c2e208b09eea6
kincade71@gmail.com <kincade71@gmail.com>
2017-04-24 23:00:54 -0400

    Cleaned up XML.

--- application/views/podcast.php
+++ application/views/podcast.php
@@ -4,21 +4,21 @@ header ("Content-Type:text/xml");
 echo '<rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
 
 	<channel>
-		<title>Higher Way Suffolk Podcast</title>
+		<title></title>
 		<itunes:subtitle></itunes:subtitle>
 		<itunes:author></itunes:author>
-		<copyright>©Higherway Suffolk </copyright>
-		<itunes:category text="Religion &amp; Spirituality">
-<itunes:category text="Christianity"/>
+		<copyright>©</copyright>
+		<itunes:category text="">
+<itunes:category text=""/>
 </itunes:category>
 <language>en-us</language>
 <itunes:image href="http://" />
-<link>http://www.higherwaysuffolk.org/</link>
+<link>http://</link>
 <description></description>
 <itunes:summary></itunes:summary>
 <itunes:owner>
-<itunes:name>WEBDMG</itunes:name>
-<itunes:email>info@webdmg.com</itunes:email>
+<itunes:name></itunes:name>
+<itunes:email></itunes:email>
 </itunes:owner>
 <itunes:explicit>no</itunes:explicit>';
 if(count($casts) > 0){
@@ -28,8 +28,8 @@ if(count($casts) > 0){
 		<itunes:subtitle>'.$cast['subtitle'].'</itunes:subtitle>
 		<itunes:author>'.$cast['author'].'</itunes:author>
 		<itunes:summary>'.$cast['summary'].'</itunes:summary>
-		<enclosure url="http://visithigherway.org/casting/podcast/'.str_replace(' ','_',$cast['title']).'.mp3" length="'.$cast['enclosure_length'].'" type="audio/mp3" />
-		<guid>http://visithigherway.org/casting/podcast/'.str_replace(' ','_',$cast['title']).'.mp3</guid>
+		<enclosure url="'.str_replace(' ','_',$cast['title']).'.mp3" length="'.$cast['enclosure_length'].'" type="audio/mp3" />
+		<guid>'.str_replace(' ','_',$cast['title']).'.mp3</guid>
 		<itunes:duration>'.$cast['duration'].'</itunes:duration>
 		<itunes:keywords>'.$cast['keywords'].'</itunes:keywords>
 		<pubDate>'.$cast['Date'].' EST</pubDate>
