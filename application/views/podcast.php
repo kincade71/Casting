d58cd9bdbd84f93f6271ffa2b3b2b7300df9220e
Richard Robinson <kincade71@gmail.com>
2018-02-13 01:31:41 -0500

    Updated xml with new tags

--- application/views/podcast.php
+++ application/views/podcast.php
@@ -1,7 +1,8 @@
 <?php
 header ("Content-Type:text/xml");
 
-echo '<rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
+echo '<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:content="http://purl.org/rss/1.0/modules/content/">
 
 	<channel>
 		<title></title>
@@ -12,28 +13,35 @@ echo '<rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.
 <itunes:category text=""/>
 </itunes:category>
 <language>en-us</language>
-<itunes:image href="http://" />
-<link>http://</link>
+<itunes:image href="" />
+<link></link>
 <description></description>
+<itunes:type>serial</itunes:type>
 <itunes:summary></itunes:summary>
 <itunes:owner>
-<itunes:name></itunes:name>
-<itunes:email></itunes:email>
+    <itunes:name></itunes:name>
+    <itunes:email></itunes:email>
 </itunes:owner>
 <itunes:explicit>no</itunes:explicit>';
 if(count($casts) > 0){
 	foreach($casts as $cast){
 		echo'<item>
-		<title>'.$cast['title'].'</title>
-		<itunes:subtitle>'.$cast['subtitle'].'</itunes:subtitle>
-		<itunes:author>'.$cast['author'].'</itunes:author>
-		<itunes:summary>'.$cast['summary'].'</itunes:summary>
-		<enclosure url="'.str_replace(' ','_',$cast['title']).'.mp3" length="'.$cast['enclosure_length'].'" type="audio/mp3" />
-		<guid>'.str_replace(' ','_',$cast['title']).'.mp3</guid>
-		<itunes:duration>'.$cast['duration'].'</itunes:duration>
-		<itunes:keywords>'.$cast['keywords'].'</itunes:keywords>
-		<pubDate>'.$cast['Date'].' EST</pubDate>
-		<itunes:explicit>'.$cast['explicit'].'</itunes:explicit>
+		<itunes:episodeType>full</itunes:episodeType>
+		<itunes:title></itunes:title>
+		<itunes:episode></itunes:episode>
+		<itunes:season></itunes:season>
+		<title></title>
+		<itunes:subtitle></itunes:subtitle>
+		<itunes:author></itunes:author>
+		<itunes:summary></itunes:summary>
+		<description></description>
+		<content:encoded></content:encoded>
+		<enclosure url="" length="" type="audio/mp3" />
+		<guid></guid>
+		<itunes:duration></itunes:duration>
+		<itunes:keywords></itunes:keywords>
+		<pubDate></pubDate>
+		<itunes:explicit></itunes:explicit>
 		</item>';
 	};
 };
